032b4e6b82fd5f0dce1e7375079341bc4d87ecc9
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-04-13 10:25:19 -0700

    test

--- index.php
+++ index.php
@@ -6,20 +6,23 @@
 <?php
 echo 'hello';
 
-global $db;
-$db=  mysql_connect('localhost','root','');
-mysql_select_db('newdatabase');
-
-if(mysqli_connect_errno())
-{
-    echo 'Could not connect to database!';
-    exit;
-}
-
-$query= "SELECT * FROM Dating_Participants";
-$result= mysql_query($query);
-print mysql_error();
-print_r(mysql_fetch_array($result));
+    echo '<p>Welcome to Hash Dating</p>';
+    global $db;
+    $db=  mysql_connect('localhost','root','');
+    mysql_select_db('newdatabase');
+    
+    if(mysqli_connect_errno())
+    {
+        echo 'Could not connect to database!';
+        exit;
+    }
+    
+    $query="SELECT * FROM Dating_Participants";
+    $result= mysql_query("$query");
+    print mysql_error();
+    print_r(mysql_fetch_array($result));
+    $num_results= $result->num_rows;
+    echo $num_results;
 
 echo 'done';
 ?>
