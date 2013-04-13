151c2ef7e472bd8b6850b4ff347cfb00b51996f1
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-04-13 05:15:09 -0700

    ijj# with '#' will be ignored, and an empty message aborts the commit.

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
             }
             echo $interest;
             
-   // $friends='SELECT uid, name, sex from user where uid in(select uid2 from friend where uid1 = me()) and sex =' . $interest;
+   $friends='SELECT uid, name, sex from user where uid in(select uid2 from friend where uid1 = me()) and sex = ' . $interest;
             
       //  $potential_partners = $facebook->api(array(
        //                                 'method' => 'fql.query',
@@ -49,8 +49,8 @@
             // FQL queries return the results in an array, so we have
             //  to get the user's name from the first element in the array.
             echo '<pre>Name: ' . $ret_obj[0]['name'] . '</pre>';
- //echo '<pre>' . $interest . 'Friend ' . $potential_partners[10]['name'] . '</pre>';
-          //  print_r($potential_partners);
+// echo '<pre>' . $interest . 'Friend ' . $potential_partners[10]['name'] . '</pre>';
+//print_r($potential_partners);
         }
         
          catch(FacebookApiException $e) {
