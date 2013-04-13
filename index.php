bc3e1507a1c3e1e63093106725d9b9b504e608a0
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-04-13 10:26:24 -0700

    test

--- index.php
+++ index.php
@@ -10,21 +10,23 @@ echo 'hello';
     global $db;
     $db=  mysql_connect('localhost','root','');
     mysql_select_db('newdatabase');
-    
+    echo 'done';
     if(mysqli_connect_errno())
     {
         echo 'Could not connect to database!';
         exit;
     }
-    
+    echo 'hi';
     $query="SELECT * FROM Dating_Participants";
+    echo 'here';
     $result= mysql_query("$query");
+    echo 'problem';
     print mysql_error();
     print_r(mysql_fetch_array($result));
     $num_results= $result->num_rows;
     echo $num_results;
 
-echo 'done';
+
 ?>
 </body>
 </html>
