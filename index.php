adad522ca072e6336d6659a367e4a70c1a5f8bdf
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-04-12 17:36:47 -0700

    j

--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 <html>
 <head>
-<title>test</title>
-<style>
-.test{
-background-color:gray;
-border: 1px solid black;
-width: 200px;
-heaight: 100px;
-}
-</style>
+
 </head>
 
-<?php
-echo '<div class="test"> Hello </div> World';
-echo 'watch this';
-?>
-</body>
+fuck you
+
 </html>
