04a8c0c13997e714c409d23ebf2c9a4313aa133d
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-04-13 05:12:20 -0700

    ii

--- index.php
+++ index.php
@@ -49,8 +49,8 @@
             // FQL queries return the results in an array, so we have
             //  to get the user's name from the first element in the array.
             echo '<pre>Name: ' . $ret_obj[0]['name'] . '</pre>';
- echo '<pre>' . $interest . 'Friend ' . $potential_partners[10]['name'] . '</pre>';
-            print_r($potential_partners);
+ //echo '<pre>' . $interest . 'Friend ' . $potential_partners[10]['name'] . '</pre>';
+          //  print_r($potential_partners);
         } catch(FacebookApiException $e) {
             // If the user is logged out, you can have a
             // user ID even though the access token is invalid.
