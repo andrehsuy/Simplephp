4997b7b1dea94354733226e5d8e4f7e11f4e0382
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-04-13 21:15:02 -0700

    test

--- index.php
+++ index.php
@@ -51,6 +51,7 @@
             global $db;
              echo "checkpoint 3";
             $db=  mysql_connect('mysql.tedx.msjhs.net','tedxmsjhs','ws8sn#N8957HB!ok');
+            echo $db;
              echo "checkpoint 4";
             mysql_select_db('tedx_msjhs');
              echo "checkpoint 5";
