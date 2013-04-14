576ec40c53173eeccd9651bc6e32ec83f7c883ac
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-04-13 21:28:16 -0700

    test

--- index.php
+++ index.php
@@ -48,9 +48,9 @@
                                             'query' => $friends,
             ));
             
-            global $db;
+            
              echo "checkpoint 3";
-            $db=  mysql_connect('mysql.tedx.msjhs.net','tedxmsjhs','ws8sn#N8957HB!ok');
+            $db=  mysql_connect('mysql.tedx.msjhs.net','tedxmsjhs@muphrid.dreamhost.com','ws8sn#N8957HB!ok');
             
             if (!$db) {
                 die('Could not connect: ' . mysql_error());
@@ -62,7 +62,6 @@
             }
             
             echo 'Connected successfully';
-             echo "checkpoint 4";
           
             
            
