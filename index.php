4260b01c93ddff8c9d69b2f37e2245add5f09d3f
Andre Hsu <andrehsu@wifi-131-179-46-184.host.ucla.edu>
2013-04-16 12:15:53 -0700

    test

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
             
              echo "checkpoint 3";
             //$db=  mysql_connect('mysql.tedx.msjhs.net','tedxmsjhs','ws8sn#N8957HB!ok');
-            $db=  mysql_connect('198.72.216.87:3306','root','');
+            $db=  mysql_connect('131.179.46.184:3306','root','');
             if (!$db) {
                 die('Could not connect: ' . mysql_error());
             }
