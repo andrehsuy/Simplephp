4f56ab7aea09f9f5b6c8070583ec58fd2372c39f
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-04-16 00:34:00 -0700

    test

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
             
              echo "checkpoint 3";
             //$db=  mysql_connect('mysql.tedx.msjhs.net','tedxmsjhs','ws8sn#N8957HB!ok');
-            $db=  mysql_connect('localhost --protocol=TCP','root','Hh1992915');
+            $db=  mysql_connect('localhost','root','');
             if (!$db) {
                 die('Could not connect: ' . mysql_error());
             }
