3b44f94db98b32c874f834e85b71baad6e1229c3
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-06-14 18:55:22 -0700

    test

--- welcome.php
+++ welcome.php
@@ -1,8 +1,8 @@
 <?php
 
-    echo 'Welcome'
+    echo 'Welcome';
     
-    $dbconn = pg_connect("host=ec2-23-21-85-233.compute-1.amazonaws.com port=5432 dbname=d3m7bds1jom9ml user=bwhsvkshzcmema password=1zleSvvgAStyG9Wv0sBri188qW sslmode=require options='--client_encoding=UTF8'") 
+    $dbconn = pg_connect("host=ec2-23-21-85-233.compute-1.amazonaws.com port=5432 dbname=d3m7bds1jom9ml user=bwhsvkshzcmema password=1zleSvvgAStyG9Wv0sBri188qW sslmode=require options='--client_encoding=UTF8'");
     
     if (!$db) {
         die('Could not connect: ' . pg_last_error());
