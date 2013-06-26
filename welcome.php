93462fe1872e12528890db63dbd7bc1b535daf45
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-06-26 12:52:55 -0700

    test

--- welcome.php
+++ welcome.php
@@ -3,59 +3,127 @@
 
     <title>Sign up form</title>
 
+    <script type="text/javascript">
+    function validate(event)
+    {
+        
+        var temp1= document.getElementById("alertEmptyFields");
+        var temp2= document.getElementById("alertPasswordMismatch");
+        var temp3= document.getElementById("alertPasswordInvalid");
+        if(temp1)
+        {
+            temp1.parentNode.removeChild(temp1);
+            
+        }
+        if(temp2)
+        {
+            temp2.parentNode.removeChild(temp2);
+        }
+        if(temp3)
+        {
+            temp3.parentNode.removeChild(temp3);
+        }
+        
+        if(event.target.firstname.value=="" || event.target.lastname.value=="" || event.target.email.value=="" || event.target.password1.value=="" || event.target.password2.value=="" || event.target.birthday.value=="")
+        {
+            var newel=document.createElement("p");
+            var t=document.createTextNode("*You must fill in all of the fields.");
+            newel.appendChild(t);
+            newel.id="alertEmptyFields";
+            document.body.appendChild(newel);
+            return false;
+        }
+        else if( event.target.password1.value != event.target.password2.value)
+        {
+            var newel=document.createElement("p");
+            var t=document.createTextNode("*Your passwords do not match.");
+            newel.appendChild(t);
+            newel.id="alertPasswordMismatch";
+            document.body.appendChild(newel);
+            return false;
+        }
+        else if( event.target.password1.value.length < 6)
+        {
+            var newel=document.createElement("p");
+            var t=document.createTextNode("*Your passwords must be at least 6 characters long.");
+            newel.appendChild(t);
+            newel.id="alertPasswordInvalid";
+            document.body.appendChild(newel);
+            return false;
+        }
+        
+        
+    }
+    </script>
+
 
     </head>
     <body>
 
 
 
-            <?php
+
+    <?php
+    
+    $dbconn = pg_connect("host=ec2-23-21-85-233.compute-1.amazonaws.com port=5432 dbname=d3m7bds1jom9ml user=bwhsvkshzcmema password=1zleSvvgAStyG9Wv0sBri188qW sslmode=require options='--client_encoding=UTF8'");
+    
+    if (!$dbconn)
+    {
+        die('Could not connect: ' . pg_last_error());
+        
+    }
+    
+    if(isset($_POST['submit']))
+    {
+        
+        $sql='INSERT INTO Users (Username, LastName, FirstName, Password, Birthday) VALUES (\''.$_POST['email'].'\',\''.$_POST['lastname'].'\',\''.$_POST['firstname'].'\',\''.$_POST['password1'].'\',\''.$_POST['birthday'].'\')';
+        
+        $insert= pg_query($dbconn, $sql);
+        
+        if(!$insert)
+        {
+            echo "Something is wrong with insert" . pg_last_error();
+        }
+        else
+        {
             
-                
+            $to      = $POST_['email'];
+            $subject = 'Email Confirmation';
+            $message = 'Testing';
+            $headers = 'From: andrehsugod@gmail.com' . "\r\n" .
+            'Reply-To: andrehsugod@gmail.com' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+            
+            mail($to, $subject, $message, $headers);
+            
     
-            $dbconn = pg_connect("host=ec2-23-21-85-233.compute-1.amazonaws.com port=5432 dbname=d3m7bds1jom9ml user=bwhsvkshzcmema password=1zleSvvgAStyG9Wv0sBri188qW sslmode=require options='--client_encoding=UTF8'");
+        }
+        
+        $result= pg_query($dbconn, "SELECT * FROM Users");
+        
+        if(!$result)
+        {
+            echo "Something is wrong with select" . pg_last_error();
+        }
+        
+        while ($row = pg_fetch_row($result)) {
+            echo "Username: $row[0]  LastName: $row[1] FirstName: $row[2] birthday:$row[4]";
+            echo "<br />\n";
+        }
+        
+        
+    }
     
-            if (!$dbconn)
-            {
-                die('Could not connect: ' . pg_last_error());
-                
-            }
-                
-                if(isset($_POST['submit']))
-                {
-                    
-                    $sql='INSERT INTO Users (Username, LastName, FirstName, Password, Birthday) VALUES (\''.$_POST['email'].'\',\''.$_POST['lastname'].'\',\''.$_POST['firstname'].'\',\''.$_POST['password1'].'\',\''.$_POST['birthday'].'\')';
-                    
-                    $insert= pg_query($dbconn, $sql);
-                    
-                    if(!$insert)
-                    {
-                        echo "Something is wrong with insert" . pg_last_error();
-                    }
-                    
-                    $result= pg_query($dbconn, "SELECT * FROM Users");
-                    
-                    if(!$result)
-                    {
-                        echo "Something is wrong with select" . pg_last_error();
-                    }
-                    
-                    while ($row = pg_fetch_row($result)) {
-                        echo "Username: $row[0]  LastName: $row[1] FirstName: $row[2] birthday:$row[4]";
-                        echo "<br />\n";
-                    }
+    ?>
 
-                     
-                }
-                
-            ?>
+<?php phpinfo(); ?>
 
-            <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+            <form id="sign_up_form" action="<?php echo $_SERVER['PHP_SELF'] ?>" onsubmit="return validate(event)" method="post">
             <input type="text" name="firstname" placeholder="First Name"><br>
             <input type="text" name="lastname" placeholder="Last Name"><br>
             <input type="text" name="email" placeholder="Email"><br>
-            <input type="text" name="password1" placeholder="New Password"><br>
-            <input type="text" name="password2" placeholder="Re-enter"><br>
+            <input type="password" name="password1" placeholder="New Password"><br>
+            <input type="password" name="password2" placeholder="Re-enter"><br>
             <input type="time" name="birthday" placeholder="Birthday"><br>
             <input type="submit" name="submit">
             </form>
@@ -63,50 +131,3 @@
 
     </body>
 </html>
-
-<?php
-
-/*
- $createTable="CREATE TABLE Users (Username varchar(255), LastName varchar(255), FirstName varchar(255), Password varchar(255), Birthday date)";
- 
- $create= pg_query($dbconn, $createTable);
- 
- if (!$create)
- {
- echo "something wrong here too" . pg_last_error();
- }
- 
- 
- $insert= pg_query($dbconn, "INSERT INTO Users VALUES('andrehsugod@ucla.edu', 'Hsu', 'Andre', 'justgoogleit','1992-09-15')");
- 
- if(!$insert)
- {
- echo "Something is wrong with insert" . pg_last_error();
- }
- 
- 
- 
- $result= pg_query($dbconn, "SELECT * FROM Users");
- 
- if(!$result)
- {
- echo "weird" . pg_last_error();
- }
- 
- while ($row = pg_fetch_row($result)) {
- echo "Username: $row[0]  LastName: $row[1]";
- echo "<br />\n";
- }
- 
- $qu= pg_query($dbconn, "SELECT * FROM Users");
- 
- $data = pg_fetch_object($qu);
- 
- 
- 
- echo $data->username;
- echo $data->lastname;
- echo $data->firstname;
- 
- */
-?>
