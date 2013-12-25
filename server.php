233a93367952e2356662f70494b1fd11479f85d7
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-12-24 22:00:55 -0800

    test

--- server.php
+++ server.php
@@ -9,7 +9,12 @@
         echo '
         
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
         
+        <script>
+        
+                
+        </script>
         
         ';
         
