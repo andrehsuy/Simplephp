5a831f1aa111e5ea0cc375ae5688b6c896d9079b
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-12-24 21:55:24 -0800

    test

--- server.php
+++ server.php
@@ -6,7 +6,7 @@
     
     if(isset($_SESSION['valid_user']))
     {
-        echo "
+        echo '
         
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
@@ -29,9 +29,9 @@
         
         </script>
         
-        ";
+        ';
         
-        echo "here";
+        echo 'here';
         
         
        
