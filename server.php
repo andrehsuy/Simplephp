ff44f4417a8697aa09dac8a9dc2be0b67f4851af
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-12-24 21:53:35 -0800

    test

--- server.php
+++ server.php
@@ -31,7 +31,7 @@
         
         ";
         
-        
+        echo "here";
         
         
        
@@ -50,39 +50,6 @@
 </head>
 
 <body>
-<?php
-if(isset($_SESSION['valid_user']))
-{
-    
-    echo "
-    <div id="canvas" style="width:750; height:750; background-color:grey; display:block; position:absolute; left:50%; top:50%; margin-left:-375; margin-top:-375">
-    
-	<img id="prada" src="prada.jpg" width="350" height="300" style="position:absolute; left:200; top:225">
-    
-    </div>
-    
-    
-    <div id= "number"> 1 </div>
-    
-    
-    <script>
-    
-    $(function()
-      {
-      $( "#prada" ).draggable({ containment: "parent", drag: function(){ setOpacity($( this ), 200, 250); }, stop: function(){ $(this).animate({left: 200, top: 225, opacity: 1}, 400); }});
-      }
-      );
-    
-    
-    </script>
-    
-
-    ";
-    
-    
-}
-
-?>
  <a href="logout.php"> logout </a>
 </body>
 </html>
