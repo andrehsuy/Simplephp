23f785a3be7842cd1ed2754d3080a8b1845e2c66
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-12-25 02:00:16 -0800

    test

--- welcome.php
+++ welcome.php
@@ -187,13 +187,11 @@ function userConnected() {
 </script>
 <script src="fb.js"></script>
 
-<script>
-document.write("&quot;"+ name + "&quot;");
-</script>
+
 
 <form id="sign_up_form" action="<?php echo $_SERVER['PHP_SELF'] ?>" onsubmit="return validate(event)" method="post" style="visibility: <?php if(isset($_POST['submit'])) echo "hidden"; else echo "none"; ?>">
 
-<input type="text" name="firstname" placeholder="First Name">
+<input type="text" name="firstname" value=<script> document.write("&quot;"+ name + "&quot;");</script> placeholder="First Name">
 <br>
 <input type="text" name="lastname" placeholder="Last Name">
 <br>
