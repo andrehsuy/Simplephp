6ce852aa877b59bcb631b90bc7645428d0da408c
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-12-24 21:56:52 -0800

    test

--- server.php
+++ server.php
@@ -7,28 +7,7 @@
     if(isset($_SESSION['valid_user']))
     {
         echo '
-        
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-        
-        <script>
-        
-        function setOpacity(target, left, boundary)
-        {
-            var diff;
-            if(target.position().left > left)
-                diff= target.position().left - left;
-            else
-                diff= left - target.position().left;
-            
-            var percentage= 1- diff/boundary;
-            $( " #number " ).text( percentage );
-            target.fadeTo(0, percentage);
-            
-        }
-        
-        </script>
-        
+        hey
         ';
         
         echo 'here';
