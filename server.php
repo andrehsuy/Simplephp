c9dfd5e8e79b53b22c7b76fbe7aaaed76c93d9a7
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-12-24 21:03:36 -0800

    newest

--- server.php
+++ server.php
@@ -1,5 +1,28 @@
 
 <html>
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+
+<script>
+
+function setOpacity(target, left, boundary)
+{
+	var diff;
+    if(target.position().left > left)
+		diff= target.position().left - left;
+	else
+		diff= left - target.position().left;
+    
+	var percentage= 1- diff/boundary;
+    $( " #number " ).text( percentage );
+	target.fadeTo(0, percentage);
+    
+}
+
+</script>
+
+</head>
 <body>
 
 <?php
@@ -10,18 +33,26 @@
         echo 'You are logged in as: '.$_SESSION['valid_user'].'<br/>';
         
         
-        <img src="prada.jpg">
+        <div id="canvas" style="width:750; height:750; background-color:grey; display:block; position:absolute; left:50%; top:50%; margin-left:-375; margin-top:-375">
+        
+        <img id="prada" src="prada.jpg" width="350" height="300" style="position:absolute; left:200; top:225">
+        
+        </div>
         
         
+        <div id= "number"> 1 </div>
         
         
+        <script>
+        
+        $(function()
+          {
+          $( "#prada" ).draggable({ containment: "parent", drag: function(){ setOpacity($( this ), 200, 250); }, stop: function(){ $(this).animate({left: 200, top: 225, opacity: 1}, 400); }});
+          }
+          );
         
         
-        
-        
-        
-        
-        
+        </script>
         
         
         echo '<a href="logout.php"> Log out </a> <br/>';
