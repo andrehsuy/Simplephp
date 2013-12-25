c9f402205a2479a024e50203a2237134a1854936
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-12-25 01:09:43 -0800

    test

--- server.php
+++ server.php
@@ -21,19 +21,47 @@
 
 function setOpacity(target, left, boundary)
 {
-	var diff;
-    if(target.position().left > left)
+    var diff;
+    var r;
+    var g;
+    var b;
+    var percentage;
+    if(target.position().left >= left)
+	{
 		diff= target.position().left - left;
+		percentage = 1- diff/boundary;
+		r= Math.round( 192*percentage );
+		b= r;
+		g= 192;
+		
+	}
 	else
+	{
 		diff= left - target.position().left;
+		percentage = 1- diff/boundary;
+		g= Math.round( 192*percentage );
+		b= g;
+		r= 192;
+	}
     
-	var percentage= 1- diff/boundary;
+	
     $( " #number " ).text( percentage );
 	target.fadeTo(0, percentage);
+	
+	target.parent().animate({ backgroundColor: "rgb("+ r + "," + g + "," + b + ")" },0);
     
 }
 
+function restore(target)
+{
+	target.animate({left: 200, top: 100, opacity: 1 }, { duration: 350, queue: false });
+	target.parent().animate({backgroundColor: "rgb(192,192,192)" }, { duration: 350, queue: false });
+}
+
+
+
 </script>
+
 
 
 
@@ -44,9 +72,9 @@ function setOpacity(target, left, boundary)
     
     echo "You are logged in as $hello" . $_SESSION['valid_user'];
     
-?>
+    ?>
 <a href="logout.php"> logout </a>
-<div id="canvas" style="width:750; height:500; background-color:grey; display:block; position:absolute; left:50%; top:50%; margin-left:-375; margin-top:-250">
+<div id="canvas" style="width:750; height:500; background-color:rgb(192,192,192); display:block; position:absolute; left:50%; top:50%; margin-left:-375; margin-top:-250">
 
 <img id="prada" src="prada.jpg" width="350" height="300" style="position:absolute; left:200; top:100">
 
@@ -60,7 +88,7 @@ function setOpacity(target, left, boundary)
 
 $(function()
   {
-  $( "#prada" ).draggable({ containment: "parent", drag: function(){ setOpacity($( this ), 200, 250); }, stop: function(){ $(this).animate({left: 200, top: 100, opacity: 1}, 400); }});
+  $( "#prada" ).draggable({ containment: "parent", drag: function(){ setOpacity($( this ), 200, 250); }, stop: function(){ restore($(this)); } });
   }
   );
 
