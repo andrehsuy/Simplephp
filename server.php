5345790eaebcc7d7926b8cae07cb1f7c514ac8ef
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-12-24 21:39:31 -0800

    test

--- server.php
+++ server.php
@@ -5,34 +5,6 @@
 
 
 helloworld
-<?php
-
-    session_start();
-    if(isset($_SESSION['valid_user']))
-    {
-    
-    echo "you are valid";
-    
-    
-    
-    <a ref="logout.php"> logout </a>
-    }
-    else
-    {
-        
-        header("Location: login.php");
-        
-        
-    }
-    
-    
-    
-    
-    
-    
-    
-?>
-
 
 </body>
 </html>
