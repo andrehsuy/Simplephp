1cefc89e36a8c0606bd7e438a44bee7981f6b0ec
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2013-12-25 02:51:40 -0800

    test

--- welcome.php
+++ welcome.php
@@ -173,7 +173,6 @@ function populatedropdown(dayfield, monthfield, yearfield){
 <div id="fb-root"></div>
 <fb:login-button id="fb-login-button" scope="publish_actions" show-faces="true" width="200"></fb:login-button><br>
 <script>
-var lname;
 var appId = 206013596249981;
 function userConnected() {
     FB.api('/me',  function(response) {
@@ -185,7 +184,46 @@ function userConnected() {
            });
 }
 </script>
-<script src="fb.js"></script>
+<script>
+
+var lname;
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+            appId      : appId,
+            status     : true, // check login status
+            cookie     : true, // enable cookies to allow the server to access the session
+            xfbml      : true  // parse XFBML
+            });
+    FB.Event.subscribe('auth.authResponseChange', function(response) {
+                       if (response.status === 'connected') {
+                       //This is what runs when the user connects to Facebook
+                       userConnected();
+                       } else if (response.status === 'not_authorized') {
+                       FB.login();
+                       } else {
+                       FB.login();
+                       }
+                       });
+};
+(function(d){
+ var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+ if (d.getElementById(id)) {return;}
+ js = d.createElement('script'); js.id = id; js.async = true;
+ js.src = "//connect.facebook.net/en_US/all.js";
+ ref.parentNode.insertBefore(js, ref);
+ }(document));
+
+
+
+
+
+
+
+
+
+</script>
 
 
 <form id="sign_up_form" action="<?php echo $_SERVER['PHP_SELF'] ?>" onsubmit="return validate(event)" method="post" style="visibility: <?php if(isset($_POST['submit'])) echo "hidden"; else echo "none"; ?>">
@@ -219,7 +257,7 @@ window.onload=function()
 </form>
 
 <script>
-document.getElementById("firstname").setAttribute("value", name);
+document.getElementById("firstname").setAttribute("value", lname);
 </script>
 
 
