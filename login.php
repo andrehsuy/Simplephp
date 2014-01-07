5041bfd33e173bbf48eb852298df5a389933db29
Andre Hsu <andrehsu@Andres-MacBook-Pro-2.local>
2014-01-07 11:52:30 -0800

    test

--- login.php
+++ login.php
@@ -1,3 +1,15 @@
+<?php
+    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+        header('WWW-Authenticate: Basic realm="My Realm"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo 'Text to send if user hits Cancel button';
+        exit;
+    } else {
+        echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
+        echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
+    }
+    ?>
+
 <form id="login" action="link.php" method="post">
 
 <input type="text" name="userid" placeholder="Username">
